9a8197b965d916fc0f4c0792db3545025a2df5b8
vidhya-1208 <karthikvidhu@gmail.com>
2025-07-19 21:09:18 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float amount = 91.0f;
+		System.out.println(amount);
+		
+		String name = "Google";
+		System.out.println(name);
+		
+		boolean isBeta = false;
+		System.out.println(isBeta);
+		
+		int releaseyear = 2008;
+		System.out.println(releaseyear);
+		
+		char logo = 'C';
+		System.out.println(logo);
+	}
+}
